c565f0e0f770347a2d3ff6ece3f9b03d96bf05df
arodoo <haro.em@hotmail.com>
2024-05-21 11:57:40 -0600

    feat: Update InfoUser component styles for user account page

--- src/components/molecules/AccountComponents/InfoUser/InfoUser.styles.js
+++ src/components/molecules/AccountComponents/InfoUser/InfoUser.styles.js
@@ -1,5 +1,24 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-
+    content: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        backgroundColor: '#f9f9f9',
+        paddingVertical: 30,
+        paddingHorizontal: 10,
+        marginTop: 30,
+    },
+    avatar: {
+        marginRight: 20,
+        backgroundColor: '#e3e3e3',
+    },
+    displayName: {
+        fontWeight: 'bold',
+        paddingBottom: 5,
+    },
+    email: {
+        paddingBottom: 5,
+    },
 })
